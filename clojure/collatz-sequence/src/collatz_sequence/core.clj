9b5ab57882221427fc655e2d77385bd94e74e641
Christopher Lopes <christopher.lopes@gmail.com>
2016-10-24 09:04:27 -0700

    Collatz sequence clojure solution

---
+++ clojure/collatz-sequence/src/collatz_sequence/core.clj
@@ -0,0 +1,29 @@
+(ns collatz-sequence.core
+  (:gen-class))
+
+(defn- even-collatz-number
+  [n]
+  (/ n 2))
+
+(defn- odd-collatz-number
+  [n]
+  (inc (* 3 n)))
+
+(defn- calculate-sequence
+  ([n] (calculate-sequence n 1))
+  ([n sequence-length]
+   (if (= 1 n)
+     sequence-length
+     (calculate-sequence
+      (if (even? n)
+        (even-collatz-number n)
+        (odd-collatz-number n))
+      (inc sequence-length)))))
+
+;; 837799 -> 525
+(defn -main
+  []
+  (->>
+   (map calculate-sequence (range 1 1000000))
+   (map-indexed vector)
+   (apply max-key second)))
