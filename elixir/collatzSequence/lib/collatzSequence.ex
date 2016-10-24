1c06d56a0405637389d15a13397ab56a304e8e75
Christopher Lopes <christopher.lopes@gmail.com>
2016-10-24 13:31:13 -0700

    Elixir solution for collatz sequence problem

---
+++ elixir/collatzSequence/lib/collatzSequence.ex
@@ -0,0 +1,30 @@
+defmodule CollatzSequence do
+  def calculate_longest_sequence do
+    Enum.map(1..1000000, fn (n) -> calculate_sequence n end)
+    |> longest_sequence_index
+    |> +(1)
+  end
+
+  defp longest_sequence_index(sequence_lengths) do
+    max = Enum.max(sequence_lengths)
+    Enum.find_index(sequence_lengths, fn (x) -> x == max end)
+  end
+
+  defp calculate_sequence(n, sequence_length \\ 1) do
+    case n do
+      1 -> sequence_length
+      _ -> calculate_sequence(
+            calculate(n),
+            (sequence_length + 1))
+    end
+  end
+
+  defp calculate(n) when (rem(n, 2) == 0) do
+    div(n, 2)
+  end
+
+  defp calculate(n) do
+    (n * 3) + 1
+  end
+
+end
---
+++ elixir/collatzSequence/test/collatzSequence_test.exs
@@ -0,0 +1,8 @@
+defmodule CollatzSequenceTest do
+  use ExUnit.Case
+  doctest CollatzSequence
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
